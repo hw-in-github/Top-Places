ca2a15514aa6df7b9ebae038308f5e7ee6fabd15
Appleseed <howandhao@163.com>
2014-08-31 23:12:18 +0800

    Initial Commit

---
+++ Top Places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Top Places
+//
+//  Created by Appleseed on 8/31/14.
+//  Copyright (c) 2014 Appleseed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Top Places/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Top Places
+//
+//  Created by Appleseed on 8/31/14.
+//  Copyright (c) 2014 Appleseed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
